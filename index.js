fc8b2555d70201d2f942ede1d374c44fce5b2000
Dominic Tarr <dominic.tarr@gmail.com>
2013-03-26 17:39:35 +0000

    work around weird edgecases

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var fixRange = require('level-fix-range')
 //get the first/last record in a range
 
-exports = module.exports = first
+exports = module.exports = peek
 exports.first    = first
 exports.last     = last
 
@@ -9,7 +9,7 @@ function once(emitter, events, listener) {
   var remove = []
   events.forEach(function (e) {
     function onEvent (arg) {
-      listener(e, arg)
+      if(listener(e, arg) === false) return
       remove.forEach(function (r) {
         r()
       })
@@ -24,9 +24,13 @@ function once(emitter, events, listener) {
 
 
 function peek (db, opts, cb) {
+  opts.limit = opts.reverse ? 2 : 1
   var stream = once(db.createReadStream(opts), 
     ['data', 'error', 'end'],
     function (event, data) {
+      if(opts.reverse && data && opts.start 
+        && (data.key.toString() > opts.start))
+        return false
       if(event == 'error') cb(data)
       else if(event == 'end') cb(new Error('range not found'), null, null)
       else cb(null, data.key, data.value)
@@ -39,6 +43,21 @@ function first (db, opts, cb) {
 }
 
 function last (db, opts, cb) {
+  var start = opts.start
   opts.reverse = true
-  return peek(db, fixRange(opts), cb)
+  return peek(db, fixRange(opts), function (err, key, value) {
+    if(err) {
+      var _start = opts.start
+      opts.start = null
+      peek (db, opts, function (_, key, value) {
+        if(!key) return cb(err, null, null)
+        var _key = key.toString()
+        if(_key <= _start && (!opts.end || _key >= opts.end))
+          cb(_, key, value)
+        else cb(err, null, null)
+      })
+    }
+    else cb(err, key, value)
+  })
 }
+
